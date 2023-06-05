9e2e6b3b542211cb52dd957dbb3e089629d645c1
kdmorjam <kdmorrison2003@yahoo.com>
2023-06-05 16:48:13 -0400

    Update logic.js

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,7 +1,7 @@
 
 let quakemap = L.map("map", {
     center:[79.38,43.65],
-    zoom: 5
+    zoom: 2
 })
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -19,17 +19,20 @@ d3.json(quakeurl).then(function (data){
     L.geoJson(data, {
         // style: function(feature) {
         //     return {
-        //         fillColor: getColor(feature.geometry.coordinates[2]),
-        //         color: "white"
+        //         //color: getColor(feature.geometry.coordinates[2])
+        //         //fillColor: getColor(feature.geometry.coordinates[2])
+        //         fillColor: "red",
+        //         color: "red"
         //     };
         // },
         pointToLayer: function(feature, latlng) {
             return new L.CircleMarker(latlng, {
-                radius: calcRadius(feature.properties.mag),
-                fillColor: getColor(feature.geometry.coordinates[2]),
-                color: "white"
-                //fillOpacity: calcFillOpacity(feature.geometry.coordinates[2]),
+                radius: calcRadius(feature.properties.mag), //adjust radius based on magnitude
+                fillColor: getColor(feature.geometry.coordinates[2]),  //get color based on depth
+                color: "white",
+                fillOpacity: 0.7   //make colors more visible
             }).bindPopup("<h2>"+feature.properties.place+"<h2><h3> Magnitude: "+feature.properties.mag+
+                        "<h3><h3> Depth: "+feature.geometry.coordinates[2]+
                         "<h3><h3> Date: "+convertToDate(feature.properties.time));
         }
        
@@ -55,32 +58,36 @@ function convertToDate(unixtime){
     return date 
 }
 
+//from leaflet tutorial
 function getColor(d) {
+    console.log(`Depth: ${d}`);
     return d > 800 ? '#800026' :
            d > 500  ? '#BD0026' :
            d > 200  ? '#E31A1C' :
            d > 100  ? '#FC4E2A' :
            d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
+           d > 20   ? '#FFA500' :
+           d > 10   ? '#FEF400' :
+                      '#AAFF89';
 }
 
+
+//taken from leaflet tutorial
 var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function (quakemap) {
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 800],
-        labels = ['<strong>Depth</strong>'];
+    var div = L.DomUtil.create('div', 'legend'),
+        depth = [0, 10, 20, 50, 100, 200, 500, 800];
+        //labels = ['<strong>Depth</strong>'];
 
+    div.innerHTML += "<h4>Depth</h4>";
     // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
+    for (var i = 0; i < depth.length; i++) {
         div.innerHTML +=
-            labels.push('<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+'));
+           '<i style="background-color:' + getColor(depth[i] + 1) + '"></i> ' +
+           depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
     }
-    div.innerHTML = labels.join('<br>');
     return div;
 };
 
